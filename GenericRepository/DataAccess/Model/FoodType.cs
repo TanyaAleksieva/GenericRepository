2bf26980da80e86fbba45087fe70c6ab86e5fd60
TanyaAleksieva <107184098+andonovvv@users.noreply.github.com>
2022-11-25 09:33:05 +0200

    Folders and classes

---
+++ GenericRepository/DataAccess/Model/FoodType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    public class FoodType
+    {
+        private int id;
+        private string name;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int FoodTypeId { get; set; }
+        public FoodType foodType { get; set; }
+    }
+}
