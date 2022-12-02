699e5442c3207e2529024156a6dcf534e4616c9d
Tanya <tanq3223@gmail.com>
2022-12-02 22:45:40 +0200

    DbContext, Repository, IRepository

---
+++ GenericRepository/DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public class IRepository<T> where T : Class
+    {
+            IEnumerable<T> GetAll
+            {
+                get;
+            }
+            void Add(T entity);
+            void Delete(T entity);
+            void Update(T entity);
+            T FindById(int Id);
+    }
+}
