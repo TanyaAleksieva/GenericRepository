17ba940e16bfafbdfccfd0cf38b0587cfaf19d75
TanyaAleksieva <107184098+andonovvv@users.noreply.github.com>
2023-01-11 08:17:37 +0200

    MVC

---
+++ GenericRepository/DataAccess/Model/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    public class Blog
+    {
+        private int id;
+        private string name;
+        private string description;
+        private int price;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+
+        public ICollection<Blog> Foods { get; set; }
+    }
+}
---
+++ GenericRepository/DataAccess/Model/BlogPostDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Model
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie.Models.Movie> Movie { get; set; }
+    }
+}
